06289a4e0f4c79f788d09d24d5061e6e09f11501
MD QUAPHIL AHMAD <115556919+Kafill-Ahmad@users.noreply.github.com>
2025-09-30 19:05:08 +0530

    Update dayone.py

--- dayone.py
+++ dayone.py
@@ -1,2 +1,18 @@
-print("Today is day one")
-print("Today is day one")
+Array=[]
+Array.append(1)
+Array.append(2)
+Array.append(3)
+Array.append(4)
+Array.append(5)
+print(Array)
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+flower=[]
+for i in range(1,101):
+    flower.append(i)
+print(flower)
+for i in range(100):
+    print(flower[i-1])
+for i in range(1,51):
+    print(i)
